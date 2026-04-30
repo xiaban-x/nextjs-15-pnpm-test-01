14f384e4984fba96cc791314ad718bcb558263a6
xiaban <980892894@qq.com>
2026-04-30 15:07:52 +0800

    003

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        12333333
+        1
         {children}
       </body>
     </html>
